6ba4db6613c2348fe3e943ac0149e1ffa75a184f
Oli Sturm <oliver@oliversturm.com>
2022-05-10 11:47:52 +0100

    Initial commit

---
+++ src/ImageItem.js
@@ -0,0 +1,13 @@
+import styles from './ImageItem.module.css';
+
+const ImageItem = ({ imageInfo }) => (
+  <div className={styles.div}>
+    <img className={styles.img} src={imageInfo.media.m} alt={imageInfo.title} />
+    <p className={styles.p}>
+      On {imageInfo.published}, {imageInfo.author} posted this image with the
+      subject "{imageInfo.title}"
+    </p>
+  </div>
+);
+
+export default ImageItem;
